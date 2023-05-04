3a908f291e6006a4ad563a3895cab40cd08b2948
Maheraj1 <maherajpatel442@gmail.com>
2023-05-04 13:57:20 +0530

    Change in external dependences and handling

--- ext/ext.mk
+++ ext/ext.mk
@@ -1,2 +1,2 @@
-EXT_LDFLAGS = -Lext/lib/glfw -Lext/lib/glad -Lext/lib/imgui -Lext/lib/stbi -Lext/lib/box2d -Lext/lib/yaml-cpp 
-EXT_CFLAGS = -Iext/include/glm -Iext/include/glfw -Iext/include/glad -Iext/include/imgui -Iext/include/stbi -Iext/include/box2d -Iext/include/yaml-cpp 
+EXT_LDFLAGS = -L../ext/lib/glfw -L../ext/lib/glad -L../ext/lib/imgui -L../ext/lib/stbi -L../ext/lib/box2d -L../ext/lib/yaml-cpp 
+EXT_CFLAGS = -I../ext/include/glm -I../ext/include/glfw -I../ext/include/glad -I../ext/include/imgui -I../ext/include/stbi -I../ext/include/box2d -I../ext/include/yaml-cpp 
